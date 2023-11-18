3bebed4e498319b387a1c93456880979c908a135
mrbrightside75 <119534646+mrbrightside75@users.noreply.github.com>
2023-11-18 16:56:05 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import MyBallot from "./MyBallot";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Login />,
+    element: <Dashboard />,
   },
   {
     path: "/dashboard",
