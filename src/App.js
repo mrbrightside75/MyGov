5649443ecf754ec16d7c33d3a8ef532da58c9f4c
mrbrightside75 <119534646+mrbrightside75@users.noreply.github.com>
2023-11-19 00:09:58 -0500

    Update to App.js new basename and Route structure

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import MyGovernment from "./MyGovernment";
 import MyRep from "./MyRep";
 import MyBallot from "./MyBallot";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-  },
-  {
-    path: "/mygovernment",
-    element: <MyGovernment />,
-  },
-  {
-    path: "/myrep",
-    element: <MyRep />,
-  },
-  {
-    path: "/myballot",
-    element: <MyBallot />,
-  },
-]);
-
 function App() {
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      <Router basename="/MyGov">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/mygovernment" element={<MyGovernment />} />
+          <Route path="/myrep" element={<MyRep />} />
+          <Route path="/myballot" element={<MyBallot />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
